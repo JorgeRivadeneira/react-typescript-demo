de32a9d6646637ec7deed7eb85a1dd6abfa822ae
Jorge <jorge.rivadeneira.cevallos@gmail.com>
2023-04-27 06:08:35 -0500

    Finalizacion del curso

--- components/RandomFox.tsx
+++ components/RandomFox.tsx
@@ -1,4 +1,15 @@
-import type { FunctionComponent, FC } from "react"
+import { FunctionComponent, FC, useEffect, useState } from "react"
+import type {ImgHTMLAttributes} from 'react';
+import { useRef } from "react"
+
+type LazyImageProps = {
+    src: string,
+    onLazyLoad?: (img: HTMLImageElement) => void
+}
+
+type ImageNative = ImgHTMLAttributes<HTMLImageElement>;
+
+type Props = LazyImageProps & ImageNative;
 
 //Implicito
 // export const RandomFox = () => {
@@ -8,12 +19,58 @@ import type { FunctionComponent, FC } from "react"
 
 //Explicitas:
 
-const random = () => Math.floor(Math.random() * 123) + 1;
+
 
 //la mas recomendada al dia de hoy: 15-Abril-2023
-export const RandomFox = (): JSX.Element => {
-    const image: string = `https://randomfox.ca/images/${random()}.jpg`
-    return <img width={320} height="auto" src={image} className="rounded" />
+export function LazyImage({
+    src,
+    onLazyLoad,
+    ...imgProps
+  }: Props): JSX.Element {
+    const node = useRef<HTMLImageElement>(null);
+    const [isLazyLoaded, setIsLazyLoaded] = useState(false);
+    const [currentSrc, setCurrentSrc] = useState(
+        "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4="
+    );
+
+    useEffect(() => {
+
+        if (isLazyLoaded) {
+            return;
+        }
+
+        //new observer
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) =>{
+                console.log(entry, node);
+                
+                if(!entry.isIntersecting || !node.current){                    
+                    return;
+                }
+
+                setCurrentSrc(src);
+                observer.disconnect();
+                setIsLazyLoaded(true);
+
+                if(typeof onLazyLoad === "function"){
+                    onLazyLoad(node.current);
+                }
+            });
+        });
+        
+        //observe node
+        if(node.current){            
+            observer.observe(node.current!) //en lugar del if, se puede usar asi: observer.observe(node.current!)
+        }
+        
+
+        //dissconect
+        return() => {
+            observer.disconnect();
+        }
+    }, [src, onLazyLoad, isLazyLoaded])
+
+    return <img ref={node} src={currentSrc} {...imgProps} />
 }
 
 
